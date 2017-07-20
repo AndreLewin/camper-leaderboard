2bbb6ba3b55366823d456f5ffd9f063c8f41e0a0
axolotl <andre.charles.lewin@gmail.com>
2017-07-20 17:24:28 +0200

    improved table, displayed last30days data (UserList component)

--- src/index.js
+++ src/index.js
@@ -2,32 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import { Grid, Row, Col } from 'react-bootstrap';
+// import { Grid, Row, Col } from 'react-bootstrap';
 
-/*
-class Input extends React.Component {
-    render() {
+
+function UserList(props) {
+    const users = props.users;
+    const listItems = users.map(function(user, index) {
         return (
-            <FormGroup>
-                <label htmlFor="markdownInputTextArea">Markdown input:</label>
-                <FormControl componentClass="textarea" id="markdownInputTextArea" className="inputField" placeholder="Write your markdown text here"/>
-            </FormGroup>
+            <tr key={index}>
+                <td>{index+1}</td>
+                <td>{user.username}   <!-- TODO Add picture --></td>
+                <td>{user.recent}</td>
+                <td>{user.alltime}</td>
+            </tr>
         );
-    }
-}
+    });
 
-class Output extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>Preview:</p>
-                <p dangerouslySetInnerHTML={{__html:this.props.markdown}} />
-            </div>
-        );
-    }
+    return <tbody>{listItems}</tbody>;
 }
-*/
-
 
 class Leaderboard extends React.Component {
     constructor() {
@@ -39,29 +31,28 @@ class Leaderboard extends React.Component {
 
     componentDidMount() {
         const urlTop100last30days = "https://fcctop100.herokuapp.com/api/fccusers/top/recent";
+        <!-- TODO fetch all time score -->
+        // Note: The JSON is already sorted on points. The app will only present and have no sorting logic.
         fetch(urlTop100last30days).then(res => res.json()).then(value => this.setState({json: value}));
     };
 
     render() {
-        console.log(this.state.json); // null for the first rendering: no data or fetching not finished
-
+        // console.log(this.state.json); // null for the first rendering: no data or fetching not finished
+        <!-- TODO Switch button -->
         return  (
-            <Grid className="Leaderboard">
-                <Row className="rowFlex">
-                    <Col xs={3} className="well colFlex">
-                        /* Rank */
-                    </Col>
-                    <Col xs={3} className="well colFlex">
-                        /* Camper Name */
-                    </Col>
-                    <Col xs={3} className="well colFlex">
-                        /* Points in the last 30 days */
-                    </Col>
-                    <Col xs={3} className="well colFlex">
-                        /* All time points */
-                    </Col>
-                </Row>
-            </Grid>
+            <div className="container">
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Camper Name</th>
+                        <th>Points in past 30 days</th>
+                        <th>All time points</th>
+                    </tr>
+                    </thead>
+                    <UserList users={this.state.json} />
+                </table>
+            </div>
         );
     }
 }
